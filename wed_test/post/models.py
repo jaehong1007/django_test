9a25b7560ede76beadce0727d66aab8deefa084b
Jaehong Kim <jaehong1007@gmail.com>
2017-10-18 10:36:43 +0900

    first commit

---
+++ wed_test/post/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    photo = models.ImageField()
+    content = models.CharField(max_length=50)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
