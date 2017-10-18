0973a9a3d07133c9d566f152f73a41be1e8bc5e6
Jaehong Kim <jaehong1007@gmail.com>
2017-10-18 10:41:40 +0900

    post app추가 post model생성

--- wed_test/post/models.py
+++ wed_test/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=20)
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to='post')
     content = models.CharField(max_length=50)
     created_date = models.DateTimeField(auto_now_add=True)
 
